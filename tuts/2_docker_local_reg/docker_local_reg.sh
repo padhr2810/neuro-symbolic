bbc7f1d04da60e866ee04db5b7ebbab976d04776
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-08-28 11:30:32 +0100

    Update docker_local_reg.sh

--- tuts/2_docker_local_reg/docker_local_reg.sh
+++ tuts/2_docker_local_reg/docker_local_reg.sh
@@ -1,3 +1,4 @@
+# set up a registry server on local network.
 
 docker run -d -p 5000:5000 -v $HOME/registry:/var/lib/registry registry:2
 # registry:2 is the name of the image - it gets pulled from docker hub the first time.
