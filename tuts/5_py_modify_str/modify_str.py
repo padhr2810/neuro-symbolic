4c899f27ab1a0e4d9d5da74c6de46716f50e9557
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-08-28 12:26:55 +0100

    Update modify_str.py

--- tuts/5_py_modify_str/modify_str.py
+++ tuts/5_py_modify_str/modify_str.py
@@ -1,10 +1,15 @@
 
+# strings are immutable 
+# so cannot simply do this: s1[0] = "x" 
+# throws an exception
+
+# APPROACH 1 TO RESOLVE THIS:
 s1 = "the"
 for c in "abc":
     s1 += c
 print(f"s1 = {s1}")
   
-
+# APPROACH 2 TO RESOLVE THIS:
 str1 = "the"
 list1 = list(str1)
 list1[2] = 'u'
