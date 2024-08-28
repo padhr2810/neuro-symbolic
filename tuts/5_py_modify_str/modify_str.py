d8616fe3a472921d7d874410315efaa118167c15
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-08-28 12:28:11 +0100

    Update modify_str.py

--- tuts/5_py_modify_str/modify_str.py
+++ tuts/5_py_modify_str/modify_str.py
@@ -3,13 +3,13 @@
 # so cannot simply do this: s1[0] = "x" 
 # throws an exception
 
-# APPROACH 1 TO RESOLVE THIS:
+# APPROACH 1 TO CIRCUMVENT THIS:
 s1 = "the"
 for c in "abc":
     s1 += c
 print(f"s1 = {s1}")
   
-# APPROACH 2 TO RESOLVE THIS:
+# APPROACH 2 TO CIRCUMVENT THIS:
 str1 = "the"
 list1 = list(str1)
 list1[2] = 'u'
