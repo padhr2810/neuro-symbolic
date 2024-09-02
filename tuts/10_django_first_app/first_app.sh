c4f851410a4aeed8dff0d4ad008cfa0a76e7e1f9
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-09-02 11:37:19 +0100

    Update first_app.sh

--- tuts/10_django_first_app/first_app.sh
+++ tuts/10_django_first_app/first_app.sh
@@ -5,4 +5,16 @@ python3 manage.py migrate
 python3 manage.py runserver
 python3 manage.py runserver 127.0.0.1:8001
 
-python manage.py startapp myblog
+python3 manage.py startapp blog
+python3 manage.py makemigrations blog
+
+# now inspect SQL output of your first migration.
+python3 manage.py sqlmigrate blog 0001
+
+python3 manage.py migrate
+
+python3 manage.py createsuperuser
+
+python3 manage.py runserver
+
+# to pp 26
