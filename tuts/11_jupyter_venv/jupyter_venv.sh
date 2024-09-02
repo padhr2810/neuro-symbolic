04b0c657784fdf0a0210eedde8eacb2a8d7626fe
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-09-02 15:29:19 +0100

    Update jupyter_venv.sh

--- tuts/11_jupyter_venv/jupyter_venv.sh
+++ tuts/11_jupyter_venv/jupyter_venv.sh
@@ -9,3 +9,4 @@ python3 -m ipykernel install --user --name env_new
 
 jupyter notebook
 # then click on "Select Kernel" in top right and the venv name should appear there.
+# This will work subsequently even if the venv has not been activated. i.e. still accessible in new jupyter notebooks
