94d83c33809748b27da8205d5b06a5cdf349c524
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-09-02 15:27:17 +0100

    Create jupyter_venv.sh

---
+++ tuts/11_jupyter_venv/jupyter_venv.sh
@@ -0,0 +1,11 @@
+# how to access a venv from within Jupyter Notebook
+
+python3 -m venv env_new
+cd env_new/bin
+source activate
+
+pip install ipykernel
+python3 -m ipykernel install --user --name env_new
+
+jupyter notebook
+# then click on "Select Kernel" in top right and the venv name should appear there.
