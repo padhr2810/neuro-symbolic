abcfae13e91977189396ee0d44ed18acdf859083
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-09-11 11:10:18 +0100

    Update hello.rs

--- tuts/26_rust_hello_world/hello.rs
+++ tuts/26_rust_hello_world/hello.rs
@@ -6,5 +6,6 @@ fn main() {
 }
 
 // println! is a macro that prints text to the console.
+// If we had called a function instead, it would be entered as println (without the !)
 // A binary can be generated using the Rust compiler: rustc
 // The hello binary can be executed from the terminal like so: ./hello
