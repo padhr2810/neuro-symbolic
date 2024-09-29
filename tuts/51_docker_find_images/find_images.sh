0b50437b782979da480bdbfbbfdeade15a7042cb
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-09-29 22:56:23 +0100

    Update find_images.sh

--- tuts/51_docker_find_images/find_images.sh
+++ tuts/51_docker_find_images/find_images.sh
@@ -1,2 +1,16 @@
 # Based on Technique 10: Find and Run a Docker Image
 
+:'
+Let’s say you’re interested in playing with Node.js. In the following example we
+searched for images matching “node” with the docker search command:
+'
+
+docker search node
+
+
+:'
+Once you’ve chosen an image, you can download it by performing a docker pull
+command on the name
+'
+
+docker pull node
