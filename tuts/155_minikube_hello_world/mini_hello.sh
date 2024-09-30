282d0b127a26c805c835dcd800c7b1ea594764c9
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-09-30 10:49:12 +0100

    Update mini_hello.sh

--- tuts/155_minikube_hello_world/mini_hello.sh
+++ tuts/155_minikube_hello_world/mini_hello.sh
@@ -1,1 +1,22 @@
-
+minikube start
+kubectl create deployment hello-node --image=registry.k8s.io/e2e-test-images/agnhost:2.39 -- /agnhost netexec --http-port=8080
+kubectl get deployments
+kubectl get pods
+kubectl get events
+kubectl config view
+kubectl get pods
+kubectl logs hello-node-55fdcd95bf-kj8wr
+kubectl expose deployment hello-node --type=LoadBalancer --port=8080
+kubectl get services
+minikube service hello-node
+minikube addons list
+minikube addons enable metrics-server
+kubectl get pod,svc -n kube-system
+kubectl top pods
+kubectl get pod,svc -n kube-system
+kubectl top pods
+minikube addons disable metrics-server
+kubectl delete service hello-node
+kubectl delete deployment hello-node
+minikube stop
+minikube delete
