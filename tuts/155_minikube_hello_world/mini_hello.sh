03e41d3f05aa7c50baae7258a8be73f08da0ad3b
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-09-30 11:45:01 +0100

    Update mini_hello.sh

--- tuts/155_minikube_hello_world/mini_hello.sh
+++ tuts/155_minikube_hello_world/mini_hello.sh
@@ -1,7 +1,27 @@
 # https://kubernetes.io/docs/tutorials/hello-minikube/
 
 minikube start
+        # creates a minikube cluster
+
+# !!! run this command in a different terminal and leave it running !!!
+minikube dashboard
+        # You can create Kubernetes resources on the dashboard such as Deployment and Service
+        # By default, the dashboard is only accessible from within the internal Kubernetes virtual network. 
+        # The dashboard command creates a temporary proxy to make the dashboard accessible from outside the Kubernetes virtual network.
+        # To stop the proxy, run Ctrl+C to exit the process. 
+        # After the command exits, the dashboard remains running in the Kubernetes cluster. 
+        # You can run the dashboard command again to create another proxy to access the dashboard.
+
+:'
+A Kubernetes Pod is a group of one or more Containers, tied together for the purposes of administration and networking. 
+The Pod in this tutorial has only one Container. 
+A Kubernetes Deployment checks on the health of your Pod and restarts the Pod's Container if it terminates. 
+Deployments are the recommended way to manage the creation and scaling of Pods.
+'
+
 kubectl create deployment hello-node --image=registry.k8s.io/e2e-test-images/agnhost:2.39 -- /agnhost netexec --http-port=8080
+        # create a Deployment that manages a Pod. The Pod runs a Container based on the provided Docker image.
+
 kubectl get deployments
 kubectl get pods
 kubectl get events
