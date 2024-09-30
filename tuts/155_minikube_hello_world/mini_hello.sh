230bf89bd84ae497d1553ed91c19e56cd3bc0d01
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-09-30 10:56:00 +0100

    Update mini_hello.sh

--- tuts/155_minikube_hello_world/mini_hello.sh
+++ tuts/155_minikube_hello_world/mini_hello.sh
@@ -1,3 +1,5 @@
+# https://kubernetes.io/docs/tutorials/hello-minikube/
+
 minikube start
 kubectl create deployment hello-node --image=registry.k8s.io/e2e-test-images/agnhost:2.39 -- /agnhost netexec --http-port=8080
 kubectl get deployments
