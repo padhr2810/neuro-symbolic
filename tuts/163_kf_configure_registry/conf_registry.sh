d67313752b3ce277da5c6e68c89c9915d72321f5
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-10-03 15:39:15 +0100

    Update conf_registry.sh

--- tuts/163_kf_configure_registry/conf_registry.sh
+++ tuts/163_kf_configure_registry/conf_registry.sh
@@ -1,6 +1,16 @@
 APPROACH 1: FROM KUBEFLOW WEBSITE:
 https://www.kubeflow.org/docs/components/pipelines/legacy-v1/installation/localcluster-deployment/
 
+export PIPELINE_VERSION=2.3.0
+kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/cluster-scoped-resources?ref=$PIPELINE_VERSION"
+kubectl wait --for condition=established --timeout=60s crd/applications.app.k8s.io
+kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/env/platform-agnostic?ref=$PIPELINE_VERSION"
+kubectl port-forward -n kubeflow svc/ml-pipeline-ui 8080:80
+
+Then, open the Kubeflow Pipelines UI at http://localhost:8080/
+
+
+
 APPROACH 2: FOR THE TREVOR GRANT KUBEFLOW BOOK:
 # is this the container builder that needs to be configured?
 #       /home/p/Documents/code/pipelines/sdk/python/kfp/containers/_container_builder.py
