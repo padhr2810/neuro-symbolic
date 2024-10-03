7d439940d37997fd08aad45179e9e8b12645cd3f
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-10-03 12:28:18 +0100

    Update dep.sh

--- tuts/156_minikube_deploy/dep.sh
+++ tuts/156_minikube_deploy/dep.sh
@@ -7,6 +7,14 @@ kubectl create deployment kubernetes-bootcamp --image=gcr.io/google-samples/kube
     # --image = ... [need full repository url for images hosted outside Docker Hub]
     
 kubectl get deployments
+
+        # Pods that are running inside Kubernetes are running on a private, isolated network. 
+        # By default they are visible from other pods and services within the same Kubernetes cluster, but not outside that network. 
+        # When we use kubectl, we're interacting through an API endpoint to communicate with our application.
+        # We will cover other options on how to expose your application outside the Kubernetes cluster later, in Module 4.
+        # The kubectl proxy command can create a proxy that will forward communications into the cluster-wide, private network. 
+        # The proxy can be terminated by pressing control-C and won't show any output while it's running.
+    
 kubectl proxy
 
 # !!! THEN IN A DIFFERENT TERMINAL !!!
