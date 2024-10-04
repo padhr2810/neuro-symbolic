3514f40b9e71051abb5209c3ae9029a67b86c923
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-10-04 13:08:34 +0100

    Update multi.sh

--- tuts/159_minikube_multiple_inst/multi.sh
+++ tuts/159_minikube_multiple_inst/multi.sh
@@ -1,1 +1,27 @@
-https://kubernetes.io/docs/tutorials/kubernetes-basics/scale/scale-intro/
+# https://kubernetes.io/docs/tutorials/kubernetes-basics/scale/scale-intro/
+# https://minikube.sigs.k8s.io/docs/tutorials/kubernetes_101/module5/
+
+kubectl get deployments
+
+kubectl get rs
+
+kubectl scale deployments/kubernetes-bootcamp --replicas=4
+
+kubectl get deployments
+
+kubectl get pods -o wide
+
+kubectl describe deployments/kubernetes-bootcamp
+
+kubectl describe services/kubernetes-bootcamp
+
+export NODE_PORT=$(kubectl get services/kubernetes-bootcamp -o go-template='{{(index .spec.ports 0).nodePort}}')
+echo NODE_PORT=$NODE_PORT
+
+curl $(minikube ip):$NODE_PORT
+
+kubectl scale deployments/kubernetes-bootcamp --replicas=2
+
+kubectl get deployments
+
+kubectl get pods -o wide
