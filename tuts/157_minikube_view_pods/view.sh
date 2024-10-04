75d7daddd9b1b609bd093cafd2cf3ede89b95e47
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-10-04 12:38:08 +0100

    Update view.sh

--- tuts/157_minikube_view_pods/view.sh
+++ tuts/157_minikube_view_pods/view.sh
@@ -23,12 +23,15 @@ kubectl describe pods
          # view what containers are inside that Pod and what images are used to build those containers
          #  details about the Pod’s container: IP address, the ports used and a list of events 
          # describe can be used for: node, pods, deployments etc.
-         
-echo -e "Starting Proxy. After starting it will not output a response. Please return to your original terminal window\n"; kubectl proxy
 
+         # Run the command below in a NEW TERMINAL WINDOW to run the proxy
+echo -e "Starting Proxy. After starting it will not output a response. Please return to your original terminal window\n"; kubectl proxy
+        # pods = we need to proxy access to them so we can debug and interact with them
+        
 export POD_NAME=$(kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
 echo Name of the Pod: $POD_NAME
 
+        # To see the output of our application, run a curl request.
 curl http://localhost:8001/api/v1/namespaces/default/pods/$POD_NAME
 
 kubectl logs $POD_NAME
