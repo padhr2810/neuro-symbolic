78924f119b1125c8d80c8ce166eb678ff60a5604
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-10-04 15:45:17 +0100

    Update multi.sh

--- tuts/159_minikube_multiple_inst/multi.sh
+++ tuts/159_minikube_multiple_inst/multi.sh
@@ -2,7 +2,9 @@
 # https://minikube.sigs.k8s.io/docs/tutorials/kubernetes_101/module5/
 
 kubectl delete service kubernetes-bootcamp
+kubectl delete deployment kubernetes-bootcamp
 
+kubectl create deployment kubernetes-bootcamp --image=gcr.io/google-samples/kubernetes-bootcamp:v1
 kubectl expose deployment/kubernetes-bootcamp --type="LoadBalancer" --port 8080
 
 kubectl get deployments
@@ -41,6 +43,7 @@ kubectl describe deployments/kubernetes-bootcamp
         # Let's check that the Service is load-balancing the traffic. To find out the exposed IP and Port we can use the describe service as we learned in the previous part of the tutorial:
 kubectl describe services/kubernetes-bootcamp
 
+export NODE_PORT=""
 export NODE_PORT=$(kubectl get services/kubernetes-bootcamp -o go-template='{{(index .spec.ports 0).nodePort}}')
 echo NODE_PORT=$NODE_PORT
 
@@ -53,5 +56,4 @@ kubectl scale deployments/kubernetes-bootcamp --replicas=2
 
         # confirm the change was deployed.
 kubectl get deployments
-
 kubectl get pods -o wide
