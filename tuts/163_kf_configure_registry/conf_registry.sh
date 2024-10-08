1166490dc5360da8e9d785bcc92a7a15f982110d
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-10-08 14:34:15 +0100

    Update conf_registry.sh

--- tuts/163_kf_configure_registry/conf_registry.sh
+++ tuts/163_kf_configure_registry/conf_registry.sh
@@ -1,6 +1,10 @@
 APPROACH 1: FROM KUBEFLOW WEBSITE:
 https://www.kubeflow.org/docs/components/pipelines/legacy-v1/installation/localcluster-deployment/
 
+# Might need to run these 2 lines FIRST:
+minikube delete
+minikube start
+
 export PIPELINE_VERSION=2.3.0
 kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/cluster-scoped-resources?ref=$PIPELINE_VERSION"
 kubectl wait --for condition=established --timeout=60s crd/applications.app.k8s.io
