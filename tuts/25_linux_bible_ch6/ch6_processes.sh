268d73edf8f81654862990c1148fb21d1fc2dbbf
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-10-23 16:59:12 +0100

    Add files via upload

--- tuts/25_linux_bible_ch6/ch6_processes.sh
+++ tuts/25_linux_bible_ch6/ch6_processes.sh
@@ -10,7 +10,7 @@ ps -ef --sort=user | less
 
 # specify column names that you want to display for processes (in quotations)
 # ps -o flag =  User-defined format ... then provide the list of column names after this.
-
+# 	process ID / username / group name / nice value / virtual memory size / resident memory size / and command name (e.g. gedit)
 ps -eo 'pid,user,group,nice,vsz,rss,comm' | less 
 
 
@@ -34,8 +34,11 @@ M
 # This is great because it allows you to pause a process without terminating it.
 # 	I used this to pause my rsync backup at 8 AM in the morning and to resume it at 6 PM (office hours) so the backup wouldn’t bother people working during the day.
 
-gedit &
+# !!!!!!!!!!!!!!!!!!!!!!!!!!   CAUTION -- THIS ONLY WORKS if gedit is NOT OPENED ALREADY
+# !!!!!!!!!!!!!!!!!!!!!!!!!!		  ENSURE YOU DIDN'T MANUALLY OPEN IT EARLIER .... 
 
+gedit &
+jobs 		# similar to 'ps' but only shows this shells jobs.
 
 default SIGTERM signal, which requests the current process to terminate gracefully
 The SIGTERM signal is a generic signal used to cause program termination. Unlike SIGKILL , this signal can be blocked, handled, and ignored. It is the normal way to politely ask a program to terminate.
@@ -43,7 +46,7 @@ The SIGTERM signal is a generic signal used to cause program termination. Unlike
 gedit &
 kill -SIGSTOP 937942	# get the process ID from previous command
 
-killall -SIGCONT gedit	# this works if you ran the "gedit &" command many times - kills them all.
+killall -SIGCONT gedit	# this RESTARTS gedit process.
 
 sudo apt-get install -y x11-apps
 xeyes &
@@ -51,9 +54,18 @@ xeyes &
 xeyes &
 killall xeyes &
 
+
+:'
+The nice and renice commands allow users to adjust the scheduling priority of processes, influencing how the Linux kernel allocates CPU time among them.
+	    ‘nice’ Command: This command is used to start a new process with a specific priority, known as the “nice value.” A higher nice value lowers the process’s priority, while a lower (negative) nice value increases it. Processes with higher priority receive more CPU time.
+  	    ‘renice’ Command: Unlike nice, which sets the priority when starting a process, renice modifies the priority of an already running process. This flexibility allows system administrators to manage process priorities based on the current system load dynamically. 
+
+'
+
 nice -n 5 gedit &
 
 renice -n 7 21578  # whatever number appeard for the previous command
 
+# ps -- show the Nice value // command name (e.g. gedit) //  process ID // username.
 ps -eo 'pid,user,nice,comm' | grep gedit
 
